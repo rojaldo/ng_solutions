582525ea131bcd6d6e5e2ad18c3440be08c384de
Daniel Rojo <danielrojo@gmail.com>
2019-09-26 12:53:08 +0200

    heroes with details and remove button for each hero

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/model/hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,17 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroName = ''
-  heroes = ['Superman', 'Spiderman', 'Batman'];
+  newHero = new Hero();
+  heroes: Hero[] = [new Hero('Superman', 'Man of steel'),
+  new Hero('Spiderman', 'Spidy'),
+  new Hero('Batman', 'Dark knight')];
   hero = '';
   constructor() { }
 
   ngOnInit() {
   }
 
-  addHero(){
-    this.heroes.push(this.heroName); 
-    this.heroName='';
+  addHero() {
+    this.heroes.push(new Hero(this.newHero.name, this.newHero.description));
+    this.newHero = new Hero();
+  }
+
+  disableButton(): boolean {
+    return(this.newHero.name === '')
+  }
+
+  removeHero(heroIndex: number) {
+    this.heroes.splice(heroIndex, 1);
   }
 
 }
