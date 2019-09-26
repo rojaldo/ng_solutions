41853c75ac128074065c7f34850c4489c50a19da
Daniel Rojo <danielrojo@gmail.com>
2019-09-26 09:56:43 +0200

    component heroes template created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { DisplayComponent } from './components/display/display.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalculatorComponent,
     DisplayComponent,
-    KeyboardComponent  
+    KeyboardComponent,
+    HeroesComponent  
   ],
   imports: [
     BrowserModule, NgbModule, FormsModule
