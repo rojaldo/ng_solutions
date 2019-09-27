08607552c860e7b8b4574d9c2fa9710bdebceac5
Daniel Rojo <danielrojo@gmail.com>
2019-09-27 09:41:07 +0200

    Apod component added, HttpClient imported to the project

--- src/app/services/apod.service.ts
+++ src/app/services/apod.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ApodService {
 
   constructor(private http: HttpClient) { }
 
-  getRequest(url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'): any {
+  getRequest(url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'): Observable<any> {
     return this.http.get(url);
   }
 }
