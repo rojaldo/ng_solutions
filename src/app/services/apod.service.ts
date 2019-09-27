4c204564fe3beb78071ecdcc5031b53c2e18bbc7
Daniel Rojo <danielrojo@gmail.com>
2019-09-27 11:32:09 +0200

    optional parameter in getRequest

--- src/app/services/apod.service.ts
+++ src/app/services/apod.service.ts
@@ -5,11 +5,11 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class ApodService {
 
-  baseURL = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY';
+  baseURL = 'https://api.nasa.gov/planetary/apod?api_key=tqz634Z1x0LiJzjbhSyUoExrZaGKLM0MG1VnROR6';
   
   constructor(private http: HttpClient) { }
 
-  getRequest(date: any): Observable<any> {
+  getRequest(date?: any): Observable<any> {
     if(date){
       return this.http.get(this.baseURL+
         '&date=' + date.year +
